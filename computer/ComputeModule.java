9aa58cb7693930ee4ebe85bd9f4caba5edf06e35
evergreenprogrammer <50580454+evergreenprogrammer@users.noreply.github.com>
2019-05-13 18:15:54 +0530

    Update ComputeModule.java

--- computer/ComputeModule.java
+++ computer/ComputeModule.java
@@ -1,7 +1,6 @@
 public class ComputeModule {
 
-static void main(String[] args){
-
-	System.out.println("This is the compute module");
-	
+	static void main(String[] args){
+		System.out.println("This is the compute module");
+	}	
 }
